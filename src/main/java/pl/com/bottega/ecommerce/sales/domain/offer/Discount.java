6f9207bc4e1a8594e1ea93d38e19d71665ced1ed
Marcin Konopka <marcin.konopka00@gmail.com>
2019-03-14 18:49:04 +0100

    add Discount class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -0,0 +1,44 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.util.Objects;
+
+public class Discount {
+    private String discountCause;
+
+    private Money discount;
+    
+    public Discount(String discountCause, Money discount) {
+        this.discountCause = discountCause;
+        this.discount = discount;
+    }
+
+    public String getDiscountCause() {
+        return discountCause;
+    }
+
+    public Money getDiscount() {
+        return discount;
+    }
+
+    public void setDiscountCause(String discountCause) {
+        this.discountCause = discountCause;
+    }
+
+    public void setDiscount(Money discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Discount discount1 = (Discount) o;
+        return Objects.equals(discountCause, discount1.discountCause) &&
+                Objects.equals(discount, discount1.discount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(discountCause, discount);
+    }
+}
